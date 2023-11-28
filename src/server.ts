be98773b099d3cfb783940e5150fab3663bc56b7
Ana Gabriela <anagabrieladev@gmil.com>
2023-11-28 20:19:02 -0300

    chore: enable cors in app

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import express from 'express';
 import helmet from 'helmet';
 import {routes} from './routes';
 import { httpLogger, logger } from './utils/logger';
+import cors from "cors";
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(httpLogger)
 app.use(express.json());
 app.use('/public', express.static('public'));
